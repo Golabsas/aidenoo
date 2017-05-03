d38ff5d8986bf0fd9da267cf3801122d385c99c7
Radoslaw Podosek <podosek.radosalw@gmail.com>
2017-05-03 13:46:32 +0200

    Ajout de maven

--- core/src/main/java/com/aidenoo/core/index/Dispatcher.java
+++ core/src/main/java/com/aidenoo/core/index/Dispatcher.java
@@ -1,5 +1,27 @@
 package com.aidenoo.core.index;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/index.do")
 public class Dispatcher extends HttpServlet {
+	private static final long serialVersionUID = -8883010093370255561L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse responce) throws IOException {
+		PrintWriter out = responce.getWriter();
+		
+		out.print("<html>");
+		out.print("<body>");
+		out.print("<h1>It Works !!!</h1>");
+		out.print("</body>");
+		out.print("</html>");
+		
+	}
 
 }
